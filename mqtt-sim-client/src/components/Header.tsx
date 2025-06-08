a160243ab3a826f672bbbd18147c77d79e6c743c
jeffreyts <tscherterj@gmail.com>
2025-06-08 15:53:22 -0500

    Removed a duplicate logo image

--- mqtt-sim-client/src/components/Header.tsx
+++ mqtt-sim-client/src/components/Header.tsx
@@ -1,6 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
 import { setTheme } from "../store/themeSlice";
-import logo from '../assets/mqttsim.svg';
 import { RootState } from "../store/store";
 
 export default function Header() {
@@ -26,7 +25,7 @@ export default function Header() {
     return (
         <header className="row justify-content-end bg-body border-bottom border-1 text-body align-items-center m-0 px-0">
             <div className="col-8 col-md-4 d-flex gap-3 align-items-center justify-content-center text-start">
-                <img src={logo} style={{width: 60, height: 'auto'}} alt="mqtt-sim logo"/>
+                <img src="/mqttsim.svg" style={{width: 60, height: 'auto'}} alt="mqtt-sim logo"/>
                 <div className="fs-2 fw-bold fst-italic">MQTT-Sim</div>
             </div>
             <div className="col-2 col-md-4 d-flex align-items-center justify-content-end px-5">
